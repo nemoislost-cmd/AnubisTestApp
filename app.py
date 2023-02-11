91b4610ecc55db6041ccbcaf01a8cab65d838fbc
= <=>
2023-02-11 12:11:53 +0800

    Update app.py
    To store sms data in a txt file. The directory on line 28 must change per person unless there's a way to do dynamically.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import json
+import os
 app = Flask(__name__)
 
 
@@ -22,6 +23,10 @@ def sms():
     sms_content = request.get_json()
     address = sms_content['smsAddress']
     body = sms_content['smsBody']
+
+    # best is to create a txt file in your desired directory, then append to said directory
+    with open(os.path.join('C:\\Users\\Jevan\\OneDrive\\Documents\\GitHub\\AnubisTestApp\\', 'smsdata.txt'), 'a') as f:
+        f.write('\n' + body)
 
     print("SMS Retrieved from Phone Number(" + address + ") : " + body)
     return "SMS Retrieved from Phone Number(" + address + ") : " + body , 200
