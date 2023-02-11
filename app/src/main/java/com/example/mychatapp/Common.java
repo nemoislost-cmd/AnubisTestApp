19a2ba82f308208fdc0faf8b957862892fb6cc0c
Brockkoli <kianyun8186@gmail.com>
2023-02-11 23:21:46 +0800

    geolocation

---
+++ app/src/main/java/com/example/mychatapp/Common.java
@@ -0,0 +1,37 @@
+package com.example.mychatapp;
+
+import android.content.Context;
+import android.location.Location;
+import android.preference.PreferenceManager;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Common {
+    public static final String KEY_REQUESTING_LOCATION_UPDATES = "LocationUpdateEnable";
+
+    public static String getLocationText(Location mLocation) {
+        return mLocation == null ? "Unknown Location" : new StringBuilder()
+                .append(mLocation.getLatitude())
+                .append("/")
+                .append(mLocation.getLongitude())
+                .toString();
+    }
+
+    public static CharSequence getLocationTitle(BackgroundService backgroundService) {
+        return String.format("Location Updated: %1$s", DateFormat.getDateInstance().format(new Date()));
+    }
+
+    public static void setRequestingLocationUpdates(Context context, boolean value) {
+        PreferenceManager.
+                getDefaultSharedPreferences(context)
+                .edit()
+                .putBoolean(KEY_REQUESTING_LOCATION_UPDATES,value)
+                .apply();
+    }
+
+    public static boolean requestingLocationUpdates(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(KEY_REQUESTING_LOCATION_UPDATES,false);
+    }
+}
